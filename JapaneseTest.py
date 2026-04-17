f6b3bb6424fa034b33e75a67d9fcc5fd8336bddc
Nathan Prevost <allahakbar18743@gmail.com>
2026-04-17 11:22:49 -0400

    did hira_kana

--- JapaneseTest.py
+++ JapaneseTest.py
@@ -192,6 +192,33 @@ def hiragana_both():
                 wrong += 1
 def hiragana_kana():
     hira_kana = hira_main | hira_dakuten | hira_combi
+    while True:
+        num_ques = num_questions()
+        if num_ques > len(hira_kana):
+            print(f"Number is bigger than question pool, enter a number between 1 and {len(hira_kana)}.")
+            continue
+        elif num_ques <= len(hira_kana):
+            break
+    wrong = 0 # nb wrong answer
+    right = 0 # nb right answer
+    count = 0 # current amount of questions asked 
+    selected = random.sample(list(hira_kana.items()), num_ques)
+    for index, (jp, en) in enumerate(selected, start=1):
+        while True:                                            # loop till right answer
+            answer = input(f"{index}. {jp}: ").strip().lower() #removes whitespaces and puts in lowercase
+            if answer == en:
+                print("Correct")
+                right += 1             # to later implement rating 
+                break
+            elif answer == "skip":
+                print("Skipping")        # later add score penalty will be applied to message
+                wrong += 1
+                break
+            elif answer == "quit":
+                exit()
+            else:
+                print("Try again")
+                wrong += 1
 def katakana_gojūon():
     pass
 def katakana_dakuten():
